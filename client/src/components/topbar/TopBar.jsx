c268f6c4d777b0e5674f0e2f79c9042d2f3df28b
Salmankhancodes <117salmankhan@gmail.com>
2021-08-25 00:36:05 +0530

    Fixed css and flex box for responsive pages

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -1,71 +1,60 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../../context/Context";
-import "./topbar.css";
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { Context } from '../../context/Context'
+import './topbar.css'
 
 export default function TopBar() {
-  const { user, dispatch } = useContext(Context);
-  const PF = "http://localhost:5000/images/"
+  const { user, dispatch } = useContext(Context)
+  const PF = 'http://localhost:5000/images/'
 
   const handleLogout = () => {
-    dispatch({ type: "LOGOUT" });
-  };
+    dispatch({ type: 'LOGOUT' })
+  }
   return (
-    <div className="top">
-      <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
+    <div className='top'>
+      <div className='topLeft'>
+        <Link to='/'>
+          <div className='topLeftIcon'>IL</div>
+        </Link>
       </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">
-            <Link className="link" to="/">
+      <div className='topCenter'>
+        <ul className='topList'>
+          <li className='topListItem'>
+            <Link className='link' to='/'>
               HOME
             </Link>
           </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              ABOUT
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/">
-              CONTACT
-            </Link>
-          </li>
-          <li className="topListItem">
-            <Link className="link" to="/write">
+          <li className='topListItem'>
+            <Link className='link' to='/write'>
               WRITE
             </Link>
           </li>
-          <li className="topListItem" onClick={handleLogout}>
-            {user && "LOGOUT"}
+          <li className='topListItem' onClick={handleLogout}>
+            {user && 'LOGOUT'}
           </li>
         </ul>
       </div>
-      <div className="topRight">
+      <div className='topRight'>
         {user ? (
-          <Link to="/settings">
-            <img className="topImg" src={PF+user.profilePic} alt="" />
+          <Link to='/settings'>
+            <img className='topImg' src={PF + user.profilePic} alt='' />
           </Link>
         ) : (
-          <ul className="topList">
-            <li className="topListItem">
-              <Link className="link" to="/login">
+          <ul className='topList'>
+            <li className='topListItem'>
+              <Link className='link' to='/login'>
                 LOGIN
               </Link>
             </li>
-            <li className="topListItem">
-              <Link className="link" to="/register">
+            <li className='topListItem'>
+              <Link className='link' to='/register'>
                 REGISTER
               </Link>
             </li>
           </ul>
         )}
-        <i className="topSearchIcon fas fa-search"></i>
+        <i className='topSearchIcon fas fa-search'></i>
       </div>
     </div>
-  );
+  )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,29 +1,30 @@
-import { useEffect, useState } from "react";
-import Header from "../../components/header/Header";
-import Posts from "../../components/posts/Posts";
-import Sidebar from "../../components/sidebar/Sidebar";
-import "./home.css";
-import axios from "axios";
-import { useLocation } from "react-router";
+import { useEffect, useState } from 'react'
+
+import Posts from '../../components/posts/Posts'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './home.css'
+import axios from 'axios'
+import { useLocation } from 'react-router'
+import Banner from '../../components/banner/Banner'
 
 export default function Home() {
-  const [posts, setPosts] = useState([]);
-  const { search } = useLocation();
+  const [posts, setPosts] = useState([])
+  const { search } = useLocation()
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("/posts" + search);
-      setPosts(res.data);
-    };
-    fetchPosts();
-  }, [search]);
+      const res = await axios.get('/posts' + search)
+      setPosts(res.data)
+    }
+    fetchPosts()
+  }, [search])
   return (
     <>
-      <Header />
-      <div className="home">
+      <Banner />
+      <div className='home'>
         <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
-  );
+  )
 }
